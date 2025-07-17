9e2c61386d9200a67e640cc9948eae5ce1eba2d0
vishans <94801273+vishans@users.noreply.github.com>
2025-07-16 21:24:23 -0400

    Partially complete parseClass() algorithm

--- test.js
+++ test.js
@@ -20,7 +20,86 @@ function parseClass(text){
     lines = lines.filter(str => str.trim() != "");
 
     console.log(lines);
+    
+    let section = '';
+    let component = '';
 
+    let index = 0;
+    let current = '';
+    let captured = false;
+    while(index < lines.length){
+        let daysNTimes = ''
+        let room = '';
+        let instructor = '';
+        let startEndDate = '';
+
+        current = lines[index];
+
+        if(/^\d{4}$/.test(current)){ // Look for class Number
+            // Next should be the section
+            section = lines[++index];
+
+            // And then component
+            component = lines[++index];
+
+            // Then days and times
+            daysNTimes = lines[++index];
+            
+            // Room (but really it's the location)
+            room = lines[++index];
+
+            // Instructor
+            instructor = lines[++index];
+
+            // Start and end date
+            startEndDate = lines[++index]
+
+            captured = true;
+
+        }else if( 
+            /[A-Za-z]{2}\s\d{1,2}:\d{2}[AP]M\s-\s\d{1,2}:\d{2}[AP]M/
+            .test(current)){
+
+            // Then days and times
+            daysNTimes = lines[index];
+            
+            // Room (but really it's the location)
+            room = lines[++index];
+
+            // Instructor
+            instructor = lines[++index];
+
+            // Start and end date
+            startEndDate = lines[++index]
+
+            captured = true;
+        }
+
+        index++;
+
+        if (captured){
+            console.log('====');
+            console.log(section);
+            console.log(component);
+            console.log(daysNTimes);
+            console.log(room);
+            console.log(instructor);
+            console.log(startEndDate);
+            console.log('====');
+            console.log('\n\n');
+        }
+
+        captured = false;
+        
+    }
+
+    // console.log(section);
+    // console.log(component);
+    // console.log(daysNTimes);
+    // console.log(room);
+    // console.log(instructor);
+    // console.log(startEndDate);
+    // console.log('\n\n\n');
 
 }
 
